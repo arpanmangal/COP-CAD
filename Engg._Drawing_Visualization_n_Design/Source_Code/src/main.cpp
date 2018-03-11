07555fe8defcbe237731aea2d5138c6f785ce3b2
Deepanshu Jindal <deepanshujindal.99@gmail.com>
2018-03-11 18:47:52 +0530

    finalised 2d input and object creation

--- Engg._Drawing_Visualization_n_Design/Source_Code/src/main.cpp
+++ Engg._Drawing_Visualization_n_Design/Source_Code/src/main.cpp
@@ -22,28 +22,40 @@ int main()
     {   
         int number_of_views;
         std::cout << "You have choosed 2D" << std::endl;
-        twoDProjection frontview,topview,sideview;
         std::cout<< "Enter how many views you wish to enter(2 or 3)" <<std::endl;
         std::cin>>number_of_views;
 
-        twoDInput frontIn,topIn,backIn;
+        twoDInput frontIn,topIn,sideIn;
 
         std::cout<< "Enter data for front view"<<std::endl;
         frontIn.inputPoints();
         frontIn.inputEdges();
+        twoDProjection * frontView;
+        frontView->add_pointSet(frontIn.getPoints());
+        frontView->add_edgeSet(frontIn.getEdges());
 
         std::cout<< "Enter data for top view"<<std::endl;
         topIn.inputPoints();
         topIn.inputEdges();
+        twoDProjection * topView;
+        topView->add_pointSet(topIn.getPoints());
+        topView->add_edgeSet(topIn.getEdges());
 
+        twoDProjection * sideView;
         if (number_of_views==3)
         {
             std::cout<< "Enter data for side view"<<std::endl;
             sideIn.inputPoints();
             sideIn.inputEdges();
+            sideView->add_pointSet(sideIn.getPoints());
+            sideView->add_edgeSet(sideIn.getEdges());
+        }
+        else
+        {
+            sideView = NULL;
         }
 
-        twoDProjection *frontview = new frontview(frontIn)
+        twoDProjectionView projectionDrawing(frontView,topView,sideView);
     }
     else
     {
